057d6307cbb5619b38229463dbf260d7386da009
Your Name <you@example.com>
2022-07-04 20:59:35 +0100

    npm packages added.

--- app.js
+++ app.js
@@ -10,9 +10,12 @@
 // Constraint
 // • Keep the input, string concatenation, and output separate.
 
+//npm install prompt-sync
+const prompt = require('prompt-sync')();
+
 let personName = prompt(`Hello what is your name?`);
 
-while(personName != null) {
+if(personName != null) {
     
     console.log(`Hello, ${personName}, nice to meet you!`);
 }
